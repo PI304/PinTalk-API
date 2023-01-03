f0ce8120dd573a59dc92e14b170ae4632b3e33d7
linda2927 <linda2927@naver.com>
2023-01-03 13:07:41 +0900

    feat: implement basic html pages for authentication

--- apps/user/html_urls.py
+++ apps/user/html_urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
+from django.views.generic import RedirectView
+
 from .html_views import home_view, profile_view
 
 urlpatterns = [
-    path("", include("allauth.urls")),
-    path("home", home_view, name="home"),
-    path("profile", profile_view, name="profile"),
+    path("", RedirectView.as_view(url="/account/login/")),
+    path("account/", include("allauth.urls")),
+    path("home/", home_view, name="home"),
+    path("profile/", profile_view, name="profile"),
 ]
