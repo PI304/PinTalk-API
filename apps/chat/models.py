804d72c2ff539fff0e4b29fdddaaa667e9ba6595
linda2927 <linda2927@naver.com>
2023-01-03 15:58:24 +0900

    feat: add Chatroom and ChatMessage model

--- apps/chat/models.py
+++ apps/chat/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+from apps.user.models import User, TimeStampMixin
+
+
+class Chatroom(TimeStampMixin):
+    id = models.BigAutoField(primary_key=True)
+    host = models.ForeignKey(User, on_delete=models.CASCADE)
+    visitor = models.CharField(max_length=20, null=False, blank=True, default="Guest")
+    last_message = models.ForeignKey("ChatMessage", on_delete=models.DO_NOTHING)
+
+    class Meta:
+        db_table = "chatroom"
+
+    def __str__(self):
+        return f"[{self.id}] {self.host}-{self.visitor}"
+
+    def __repr__(self):
+        return f"Chatroom({self.id}, {self.host}, {self.visitor})"
+
+
+class ChatMessage(TimeStampMixin):
+    id = models.BigAutoField(primary_key=True)
+    chatroom = models.ForeignKey(Chatroom, on_delete=models.CASCADE)
+    message = models.CharField(max_length=2000, null=False)
+    is_host = models.BooleanField(null=False, blank=False)
+
+    class Meta:
+        db_table = "chat_message"
+
+    def __str__(self):
+        return f"[{self.id}] chatroom: {self.chatroom_id}"
+
+    def __repr__(self):
+        return f"ChatMessage({self.id}, {self.chatroom_id})"
