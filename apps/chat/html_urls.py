fea593826e85de5529a610176175d5392f15b030
linda2927 <linda2927@naver.com>
2023-01-05 21:17:43 +0900

    fix: delete unnecessary theme app

--- apps/chat/html_urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from django.views.generic import RedirectView
-
-from .html_views import chatroom_view
-
-urlpatterns = [
-    path("", chatroom_view),
-]
--- apps/chat/html_views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def chatroom_view(request):
-    return render(request, "chat/chatroom.html")
