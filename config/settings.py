dea2a84fd6bb9a90a2453d711d2b65227df79962
linda2927 <linda2927@naver.com>
2023-01-06 12:42:22 +0900

    feat: connect mysql as db backend

--- config/settings.py
+++ config/settings.py
@@ -159,9 +159,15 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": "django.db.backends.mysql",
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "CONN_MAX_AGE": 60 * 10,  # 10 minutes
+        "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+    },
 }
 
 
