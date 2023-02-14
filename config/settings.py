7cc5ac16a7a5012d3aa67e20a7d5401f80795dc0
linda2927 <linda2927@naver.com>
2023-02-14 18:06:40 +0900

    fix: django backend db configurations

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["43.201.60.12", "localhost", "127.0.0.1"]
 
 
 # Application definition
