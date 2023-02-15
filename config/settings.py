cd589ff59a77e130aae3f62705da453daf1d542f
linda2927 <linda2927@naver.com>
2023-02-16 07:29:52 +0900

    fix: add environment variables for django app

--- config/settings.py
+++ config/settings.py
@@ -173,7 +173,7 @@ DATABASES = {
         "PORT": 3306,
         "NAME": "pintalk",
         "USER": "root",
-        "HOST": "0.0.0.0",
+        "HOST": "pintalk-db",
         "PASSWORD": os.environ.get("DB_PASSWORD"),
         "CONN_MAX_AGE": 60 * 10,  # 10 minutes
         "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
