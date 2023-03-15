1d8de6ed1b8907072585d218974f6e857e82ab5e
linda2927 <linda2927@naver.com>
2023-03-15 16:59:24 +0900

    feat: enable session cookie for all domains

--- config/settings/base.py
+++ config/settings/base.py
@@ -153,7 +153,7 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # SESSION
 SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
 SESSION_SAVE_EVERY_REQUEST = True
-SESSION_COOKIE_DOMAIN = ["http://localhost:3000"]
+SESSION_COOKIE_DOMAIN = None
 SESSION_COOKIE_HTTPONLY = True
 # SESSION_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = False
