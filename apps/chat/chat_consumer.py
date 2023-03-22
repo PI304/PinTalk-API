9c361cf3710afc294d0e678802c19ff09a743fc8
linda2927 <linda2927@naver.com>
2023-03-23 08:47:46 +0900

    refac: make custom base consumer class extending AsyncJsonWebsocketConsumer

--- apps/chat/chat_consumer.py
+++ apps/chat/chat_consumer.py
@@ -1,50 +1,34 @@
 import datetime
 import os
-from urllib.parse import unquote, quote
-from enum import Enum
 from dotenv import load_dotenv
 
 import redis
 from channels.db import database_sync_to_async
 from channels.exceptions import DenyConnection
 
-from channels.generic.websocket import AsyncJsonWebsocketConsumer
-from django.contrib.auth.models import AnonymousUser
 from django.http import Http404
 from django.shortcuts import get_object_or_404
-from django.utils import timezone
 
+from apps.chat.base_consumer import BaseJsonConsumer, UserType
 from apps.chat.models import Chatroom
-from apps.chat.serializers import ChatMessageSerializer
 from apps.chat.services import ChatroomService
-from apps.user.models import User
 
 load_dotenv()
 
 
-class UserType(Enum):
-    GUEST = 0
-    USER = 1
+class ChatConsumer(BaseJsonConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__("room_name", "chat", *args, **kwargs)
 
-
-class ChatConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
-        self.user = self.scope["user"]
+        await super().connect()
 
-        if isinstance(self.user, AnonymousUser):
-            # Check Origin
-            host = await self.get_host_by_origin_header()
-            self.user_type = UserType.GUEST
-            self.host = host
-        else:
-            self.user_type = UserType.USER
+        try:
+            chatroom = await self.get_chatroom_instance()
+            self.chatroom = chatroom
+        except Http404:
+            await self.close(code=4004)
 
-        # Check Chatroom
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = "chat_%s" % self.room_name
-
-        chatroom = await self.get_chatroom_instance()
-        self.chatroom = chatroom
         if chatroom.is_closed:
             # chatroom 이 종료된 상태일 때
             await self.reopen_chatroom()
@@ -82,13 +66,7 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
         if hasattr(self, "room_group_name"):
             await self.save_latest_message()
 
-        try:
-            # Leave room group
-            await self.channel_layer.group_discard(
-                self.room_group_name, self.channel_name
-            )
-        except Exception as e:
-            print("Failed to leave group")
+        await super().disconnect(close_code)
 
     # Receive message from WebSocket
     async def receive_json(self, content, **kwargs):
@@ -124,22 +102,6 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
         await self.send_json(event)
 
     @database_sync_to_async
-    def get_host_by_origin_header(self):
-        origin = None
-        for header_tuple in self.scope["headers"]:
-            if bytes("origin", "utf-8") in header_tuple:
-                origin = header_tuple[1].decode("utf-8").split("//")[1]
-
-        if not origin:
-            raise DenyConnection("Origin header missing")
-
-        try:
-            registered_user = get_object_or_404(User, service_domain=origin)
-        except Http404:
-            raise DenyConnection("Request origin not registered")
-        return registered_user
-
-    @database_sync_to_async
     def save_latest_message(self) -> None:
         latest_message = ChatroomService.get_latest_message(
             self.room_group_name, self.conn
@@ -165,9 +127,6 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
 
     @database_sync_to_async
     def get_chatroom_instance(self) -> Chatroom:
-        try:
-            chatroom = get_object_or_404(Chatroom, name=self.room_name)
-        except Http404:
-            raise DenyConnection("Cannot get Chatroom instance by provided room name")
+        chatroom = get_object_or_404(Chatroom, name=self.room_name)
 
         return chatroom
