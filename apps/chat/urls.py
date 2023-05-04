a605e55114d1914f0ddceb79b9685232857b2521
linda2927 <linda2927@naver.com>
2023-05-05 08:41:45 +0900

    feat: add endpoint for restoring closed chatrooms

--- apps/chat/urls.py
+++ apps/chat/urls.py
@@ -1,28 +1,27 @@
 from django.urls import path
-from .views import (
-    ChatroomListView,
-    ChatroomDetailView,
-    ChatroomExportView,
-    ChatroomClientCreateView,
-    ChatroomMessageView,
-)
+from apps.chat import views
 
 urlpatterns = [
-    path("", ChatroomClientCreateView.as_view(), name="create-chatroom"),
-    path("chatrooms/", ChatroomListView.as_view(), name="chatroom-list"),
+    path("", views.ChatroomClientCreateView.as_view(), name="create-chatroom"),
+    path("chatrooms/", views.ChatroomListView.as_view(), name="chatroom-list"),
     path(
         "chatrooms/<int:pk>/chat-messages/",
-        ChatroomMessageView.as_view(),
+        views.ChatroomMessageView.as_view(),
         name="chatroom-messages",
     ),
     path(
         "chatrooms/<int:pk>/",
-        ChatroomDetailView.as_view(),
+        views.ChatroomDetailView.as_view(),
         name="chatroom-detail",
     ),
     path(
         "chatrooms/<int:pk>/download/",
-        ChatroomExportView.as_view(),
+        views.ChatroomExportView.as_view(),
         name="export-chatroom-messages",
+    ),
+    path(
+        "chatrooms/<int:pk>/restore/",
+        views.ChatroomRestoreView.as_view(),
+        name="restore-chatroom",
     ),
 ]
