006ab2779fb97ed9278f3e5f6c0294d93abcdc0d
linda2927 <linda2927@naver.com>
2023-05-05 10:53:51 +0900

    refac: add RedisService class for manipulating redis data and use this class at ChatConsumerService and StatusConsumerService

--- apps/chat/services.py
+++ apps/chat/services.py
@@ -2,12 +2,12 @@ import json
 import os
 import uuid
 from datetime import datetime
-from io import StringIO
-from typing import Union, List
+from typing import Union, List, Optional
 from dotenv import load_dotenv
 
 import redis
 import shortuuid
+from redis.client import Redis
 from rest_framework.request import Request
 
 from apps.chat.models import Chatroom, ChatMessage
@@ -19,6 +19,52 @@ from apps.chat.serializers import (
 from config.exceptions import InvalidInputException
 
 load_dotenv()
+
+
+class RedisService:
+    def __init__(self, redis_conn: Redis):
+        self.redis_conn = redis_conn
+
+    def get_latest_obj(self, key: str) -> Union[dict, None]:
+        latest_message = self.redis_conn.zrevrangebyscore(
+            key,
+            datetime.now().strftime("%Y%m%d%H%M%S"),
+            "-9999999999",
+            start=0,
+            num=1,
+        )
+        if len(latest_message) == 0:
+            return None
+        else:
+            json_dict = latest_message[0].decode("utf-8")
+            return dict(json.loads(json_dict))
+
+    def save_obj(self, key: str, msg_obj: dict) -> dict:
+        serializer = ChatMessageInMemorySerializer(data=msg_obj)
+        if serializer.is_valid(raise_exception=True):
+            data = serializer.data
+            json_msg = json.dumps(data, ensure_ascii=False).encode("utf-8")
+            score = self.datetime_str_to_score_format(data["datetime"])
+            self.redis_conn.zadd(
+                key,
+                {json_msg: score},
+            )
+            return serializer.validated_data
+
+    def empty_sorted_set(self, key: str) -> None:
+        self.redis_conn.zremrangebyrank(key, 0, -1)
+
+    def remove_key(self, key: str) -> None:
+        self.redis_conn.delete(key)
+
+    @staticmethod
+    def datetime_str_to_score_format(datetime_str: str) -> str:
+        try:
+            datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M:%S")
+        except ValueError:
+            raise ValueError("Incorrect data format, should be YYYY-MM-DDTHH:MM:SS")
+
+        return datetime_str.replace("-", "").replace("T", "").replace(":", "")
 
 
 class ChatroomService(object):
@@ -35,120 +81,6 @@ class ChatroomService(object):
         u = uuid.uuid1()
         s = shortuuid.encode(u)
         return s
-
-    @staticmethod
-    def save_msg_in_mem(msg_obj: dict, group_name: str, redis_conn=None) -> ChatMessage:
-        if redis_conn is None:
-            rd = redis.StrictRedis(host=os.environ.get("REDIS_HOST"), port=6379, db=0)
-        else:
-            rd = redis_conn
-
-        serializer = ChatMessageInMemorySerializer(data=msg_obj)
-        if serializer.is_valid(raise_exception=True):
-            data = serializer.data
-            json_msg = json.dumps(data, ensure_ascii=False).encode("utf-8")
-            score = data["datetime"].replace("-", "").replace("T", "").replace(":", "")
-            rd.zadd(
-                group_name,
-                {json_msg: score},
-            )
-            return serializer.validated_data
-
-    @staticmethod
-    def get_past_messages(
-        group_name: str, redis_conn, starting_point: Union[str, None] = None
-    ) -> List[dict]:
-        if starting_point is None:
-            from_score = datetime.now().strftime("%Y%m%d%H%M%S")
-            a_week = str(int(from_score) - 7000000)
-        else:
-            try:
-                from_score = datetime.strptime(starting_point, "%Y-%m-%dT%H:%M:%S")
-            except ValueError:
-                raise InvalidInputException(
-                    "Incorrect data format, should be YYYY-MM-DDTHH:MM:SS"
-                )
-
-            from_score = int(str(from_score)) - 1
-            a_week = str(from_score - 7000000)
-
-        messages = redis_conn.zrangebyscore(group_name, a_week, from_score, 0, 50)
-
-        decoded_messages: List[dict] = []
-
-        for m in messages:
-            json_dict = m.decode("utf-8")
-            decoded_messages.append(dict(json.loads(json_dict)))
-        return decoded_messages
-
-    @staticmethod
-    def get_latest_message(group_name: str, redis_conn) -> Union[None, dict]:
-        latest_message = redis_conn.zrevrangebyscore(
-            group_name,
-            datetime.now().strftime("%Y%m%d%H%M%S"),
-            "-9999999999",
-            start=0,
-            num=1,
-        )
-        if len(latest_message) == 0:
-            return None
-        else:
-            json_dict = latest_message[0].decode("utf-8")
-            return dict(json.loads(json_dict))
-
-    @staticmethod
-    def save_latest_message(
-        chatroom: Chatroom, latest_msg_obj: dict, is_guest: bool = False
-    ) -> dict:
-        data = dict(
-            latest_msg_at=latest_msg_obj.get("datetime"),
-            latest_msg=latest_msg_obj.get("message"),
-        )
-        if not is_guest:
-            data["last_checked_at"] = datetime.now()
-
-        serializer = ChatroomSerializer(chatroom, data=data, partial=True)
-
-        if serializer.is_valid(raise_exception=True):
-            serializer.save(updated_at=datetime.now())
-
-        print(serializer.data)
-        return serializer.data
-
-    @staticmethod
-    def delete_chatroom_messages_mem(room_name: str, redis_conn=None) -> None:
-        if redis_conn is None:
-            rd = redis.StrictRedis(host=os.environ.get("REDIS_HOST"), port=6379, db=0)
-        else:
-            rd = redis_conn
-
-        rd.zremrangebyrank("chat_" + room_name, 0, -1)
-
-    @staticmethod
-    def save_message(
-        chatroom_id: int,
-        msg_obj: dict,
-    ) -> dict:
-        serializer = ChatMessageSerializer(data=msg_obj)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save(chatroom_id=chatroom_id)
-
-        return serializer.data
-
-    def _get_all_messages_in_mem(self):
-        group_name = f"chat_{self.chatroom.name}"
-        redis_conn = redis.StrictRedis(
-            host=os.environ.get("REDIS_HOST"), port=6379, db=0
-        )
-
-        messages = redis_conn.zrange(group_name, 0, -1, withscores=True)
-
-        decoded_messages: List[dict] = []
-
-        for m in messages:
-            json_dict = m[0].decode("utf-8")
-            decoded_messages.append(dict(json.loads(json_dict)))
-        return decoded_messages
 
     def get_file_text(self) -> str:
         msg_data = ""
@@ -172,30 +104,113 @@ class ChatroomService(object):
 
         return msg_data
 
-
-class ChatroomStatusService:
-    @staticmethod
-    def save_status_in_mem(msg_obj: dict, group_name: str, redis_conn) -> None:
-        if redis_conn is None:
-            rd = redis.StrictRedis(host=os.environ.get("REDIS_HOST"), port=6379, db=0)
-        else:
-            rd = redis_conn
-
-        json_msg = json.dumps(msg_obj, ensure_ascii=False).encode("utf-8")
-        rd.zadd(
-            group_name,
-            {
-                json_msg: datetime.fromtimestamp(msg_obj["timestamp"]).strftime(
-                    "%Y%m%d%H%M%S"
-                )
-            },
+    def _get_all_messages_in_mem(self):
+        group_name = f"chat_{self.chatroom.name}"
+        redis_conn = redis.StrictRedis(
+            host=os.environ.get("REDIS_HOST"), port=6379, db=0
         )
 
-    @staticmethod
-    def delete_status_room_mem(group_name: str, redis_conn=None) -> None:
-        if redis_conn is None:
-            rd = redis.StrictRedis(host=os.environ.get("REDIS_HOST"), port=6379, db=0)
-        else:
-            rd = redis_conn
+        messages = redis_conn.zrange(group_name, 0, -1, withscores=True)
 
-        rd.delete(group_name)
+        decoded_messages: List[dict] = []
+
+        for m in messages:
+            json_dict = m[0].decode("utf-8")
+            decoded_messages.append(dict(json.loads(json_dict)))
+        return decoded_messages
+
+
+class ChatConsumerService:
+    def __init__(
+        self, group_name: str, chatroom: Chatroom, redis_conn: Optional[Redis]
+    ):
+        self.redis_service = RedisService(redis_conn)
+        self.group_name = group_name
+        self.chatroom = chatroom
+        if redis_conn is None:
+            self.redis_conn = redis.StrictRedis(
+                host=os.environ.get("REDIS_HOST"), port=6379, db=0
+            )
+        else:
+            self.redis_conn = redis_conn
+
+    def save_msg_in_mem(self, msg_obj: dict) -> dict:
+        return self.redis_service.save_obj(self.group_name, msg_obj)
+
+    def get_past_messages(self, starting_point: Optional[str]) -> List[dict]:
+        if starting_point is None:
+            from_score = datetime.now().strftime("%Y%m%d%H%M%S")
+            a_week = str(int(from_score) - 7000000)
+        else:
+            try:
+                from_score = datetime.strptime(starting_point, "%Y-%m-%dT%H:%M:%S")
+            except ValueError:
+                raise InvalidInputException(
+                    "Incorrect data format, should be YYYY-MM-DDTHH:MM:SS"
+                )
+
+            from_score = int(str(from_score)) - 1
+            a_week = str(from_score - 7000000)
+
+        messages = self.redis_conn.zrangebyscore(
+            self.group_name, a_week, from_score, 0, 50
+        )
+
+        decoded_messages: List[dict] = []
+
+        for m in messages:
+            json_dict = m.decode("utf-8")
+            decoded_messages.append(dict(json.loads(json_dict)))
+        return decoded_messages
+
+    def get_latest_message(self) -> Union[None, dict]:
+        return self.redis_service.get_latest_obj(self.group_name)
+
+    def save_latest_message_db(
+        self, latest_msg_obj: dict, is_guest: bool = False
+    ) -> dict:
+        data = dict(
+            latest_msg_at=latest_msg_obj.get("datetime"),
+            latest_msg=latest_msg_obj.get("message"),
+        )
+        if not is_guest:
+            data["last_checked_at"] = datetime.now()
+
+        serializer = ChatroomSerializer(self.chatroom, data=data, partial=True)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(updated_at=datetime.now())
+
+        return serializer.data
+
+    def delete_chatroom_messages_mem(self) -> None:
+        self.redis_service.remove_key(self.group_name)
+
+    def save_chat_message_db(self, msg_obj: dict) -> dict:
+        serializer = ChatMessageSerializer(data=msg_obj)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(chatroom_id=self.chatroom.id)
+
+        return serializer.data
+
+
+class StatusConsumerService:
+    def __init__(self, group_name: str, redis_conn: Optional[Redis]):
+        self.redis_service = RedisService(redis_conn)
+        self.group_name = group_name
+        if redis_conn is None:
+            self.redis_conn = redis.StrictRedis(
+                host=os.environ.get("REDIS_HOST"), port=6379, db=0
+            )
+        else:
+            self.redis_conn = redis_conn
+
+    def get_latest_status(self) -> Union[None, dict]:
+        return self.redis_service.get_latest_obj(self.group_name)
+
+    def update_status_in_mem(self, msg_obj: dict) -> dict:
+        self.redis_service.empty_sorted_set(self.group_name)
+        return self.redis_service.save_obj(self.group_name, msg_obj)
+
+    def delete_status_room_mem(self) -> None:
+        self.redis_service.remove_key(self.group_name)
